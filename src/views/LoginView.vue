6881fd86b916e445e04b6a1f9dc00f87d6b9db1f
MQ developer <matias.queirolo10@gmail.com>
2022-09-14 06:11:32 -0300

    Se realiza el logi

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="menu">
+    <img alt="Urbano Fitness logo" src="../assets/Urban.png" class="img">
+    <form class="px-4 py-3">
+      <div class="mb-3">
+        <label class="form-label">User Name</label>
+        <input type="user" class="form-control"  placeholder="Matias123">
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" class="form-control" placeholder="Password">
+      </div>
+      <br>
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
+
+
+  </div>
+</template>
+
+<!-- <style>
+
+  img{
+    border-radius: 50%;
+  }
+</style> -->
