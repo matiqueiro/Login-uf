3492069e037e91e86ee218ee717744e93d5d4a98
Matias Queirolo <matias.queirolo10@gmail.com>
2022-09-23 18:14:36 -0300

    login coneccion jwt

---
+++ README.md
@@ -0,0 +1,25 @@
+![screencapture-localhost-8080-2022-09-14-05_58_13](https://user-images.githubusercontent.com/72896152/190263720-3bc7ef2a-f2c0-4f93-a1e7-134d578f8efa.png)
+# uf-login
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/LoginView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login">
+    <img alt="Urbano Fitness logo" src="../assets/Urban.png" class="img">
+    <form class="px-4 py-3" v-on:submit.prevent="login">
+      <div class="mb-3">
+        <label class="form-label">User Name</label>
+        <input type="text" class="form-control"  placeholder="Matias123" id="user" v-model="user">
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" class="form-control" placeholder="Password" id="password" v-model="password">
+      </div>
+      <br>
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
+
+
+  
+  </div>
+</template>
+<script>
+  //importa axios
+  export default{
+  name: 'LoginView',
+  components: {
+    
+  },
+  data: function(){
+    return{    
+    user: "",
+    password: ""
+    }
+  }, 
+  methods:{
+    login() {
+      let json = {
+        "user" : this.user,
+        "password" : this.password
+      };
+      fetch('http://127.0.0.1:8000/api/auth/login', {
+        "method": "post",
+      })
+       .then(response => response.json())
+       .then(data => console.log(data))
+    
+    }
+  }
+  }
+</script>
